50648bdf5c1277dc749957e9d8e0ac4b8b52814a
unknown <agnijwala123@gmail.com>
2022-05-07 22:18:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <h1>Registration Form</h1>
+        <div className="ui divider"></div>
+        <div className="ui form"></div>
+
+          <div className="field"></div>
+             <label>Username</label>
+             <input type="text" name="username" placeholder="Username" />
+
+          <div className="field"></div>
+           <label>Email</label>
+          <input  name="Email" placeholder="Email" />
+
+          <div className="field"></div>
+             <label>Password</label>
+             <input type="Password" name="Password" placeholder="Password" />
+
+          <button className="fluid ui button blue">Submit</button>
+      </form>
     </div>
   );
 }
